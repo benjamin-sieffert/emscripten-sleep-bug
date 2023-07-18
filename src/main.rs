53c0687bb41232a94335166b3ed34289d68cbc50
Benjamin Sieffert <109514252+benjamin-sieffert@users.noreply.github.com>
2023-07-18 17:44:45 +0200

    better report

--- src/main.rs
+++ src/main.rs
@@ -92,26 +92,25 @@ fn main() {
         if frame_time < u32::MAX as u128 {
             let fps = 1_000u32 / 60u32;
             if fps > frame_time as u32 {
-                let sleep_start = Instant::now();
                 let mut sleep_for = fps - frame_time as u32;
 
+                let full_sleep_for = sleep_for;
+                let mut early_wake = 0;
+
                 loop {
+                    let sleep_start = Instant::now();
+
                     #[cfg(target_os = "emscripten")]
                     emscripten::sleep(sleep_for);
 
                     #[cfg(not(target_os = "emscripten"))]
-                    {
-                        std::thread::sleep(Duration::from_millis(sleep_for as u64));
-                    }
+                    std::thread::sleep(Duration::from_millis(sleep_for as u64));
 
                     let elapsed = sleep_start.elapsed().as_millis() as u32;
 
                     if elapsed < sleep_for {
-                        // println!(
-                        //     "woke up too early! wanted to sleep for {}, but slept for {}",
-                        //     sleep_for, elapsed
-                        // );
                         sleep_for -= elapsed;
+                        early_wake += 1;
                         continue;
                     }
 
@@ -123,6 +122,13 @@ fn main() {
                     }
 
                     break;
+                }
+
+                if early_wake > 2 {
+                    eprintln!(
+                        "woke up early {} times while trying to sleep {} ms",
+                        early_wake, full_sleep_for
+                    );
                 }
             }
         }
