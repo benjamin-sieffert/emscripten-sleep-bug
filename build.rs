72e969768504144b8974738ab543e555ac3f64f8
Benjamin Sieffert <109514252+benjamin-sieffert@users.noreply.github.com>
2023-07-20 09:17:03 +0200

    fully remove sdl compilation to be faster

--- build.rs
+++ build.rs
@@ -8,9 +8,9 @@ fn main() {
     }
 
     let link_args = [
-        "USE_SDL=2",
-        "USE_SDL_TTF=2",
-        "ALLOW_MEMORY_GROWTH=1",
+        // "USE_SDL=2",
+        // "USE_SDL_TTF=2",
+        // "ALLOW_MEMORY_GROWTH=1",
         "ASYNCIFY=1",
     ];
 
@@ -18,5 +18,5 @@ fn main() {
         println!("cargo:rustc-link-arg=-s{}", x);
     }
 
-    println!("cargo:rustc-link-arg=--embed-file=assets");
+    // println!("cargo:rustc-link-arg=--embed-file=assets");
 }
